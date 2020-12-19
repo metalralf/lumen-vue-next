e9b793bdcfdd163d294f0572fe4d8709936d8edf
Kapitány Rudolf <kapitanyrudolf@gmail.com>
2020-12-19 15:20:50 +0100

    laravel mix, vue3 and basic scaffolding added

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <div id="app">
+        <App></App>
+    </div>
+
+    <script src="js/app.js"></script>
+</body>
+</html>
